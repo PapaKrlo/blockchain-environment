fecc2c2277b556ae583c1b4d2fcb77bd9b01f971
Carlos Leonardo Emanuele <carloslemanuele@gmail.com>
2022-05-13 12:07:19 +0100

    Blockchain Environment Done!
    With everything (including sound)

---
+++ Assets/Scripts/CursorHide.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHide : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //Set Cursor to be not visible
+        Cursor.visible = false;
+    }
+}
